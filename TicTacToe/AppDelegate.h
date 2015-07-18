3bbe85604d52525c5454b097e97e1d4ae70e91a6
Rachel Schneebaum <reschneebaum@gmail.com>
2015-07-18 09:58:05 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Rachel Schneebaum on 7/18/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
